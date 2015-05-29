002a2cec9379f310e3ded96cf0a240490eb5de77
Joe Violago <joe@violago.net>
2015-05-29 09:28:19 -0400

    featurama init; WIP

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-bhousman.github.io
+josephviolago.github.io
 ==================
 
-Honest Buildings Huddle
+Featurama standup chant
+
+## Resources
+
+### JS
+http://code.jquery.com/jquery-2.1.4.min.js
+
+### Images
+http://upload.wikimedia.org/wikipedia/commons/8/84/Futurama_1999_logo.svg
+http://spot.pcc.edu/~gkerr/paststudents/matt_shettles/mysite/images/ship-large.png
+http://theinfosphere.org/images/f/fc/Planet_Express_Logo.svg
+http://datasplasher.stvisuals.com/media-hostpage/FUTURAMA-BACKGROUND-small.jpg
+http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg
+
+### Audio
+https://files.slack.com/files-pri/T02T0838D-F04SHATNS/download/futuramathemesong.mp3
+
+### Font
+http://csswarp.eleqtriq.com/
+http://thefonty.com/font/futurama/download
+http://www.cufonfonts.com/en/font/4962/futurama
--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 (function() {
 
     var logo = document.getElementById('logo'),
-        audio = document.getElementById('audio'),
-        body = document.body;
+        audio = document.getElementById('audio')
+        mute = document.getElementById('mute')
+        select = document.getElementById('nav');
 
-    function play() {
+    function audioPlay() {
         if (audio.paused) {
             audio.play();
         } else {
-            audio.currentTime = 0;
+            audioReset();
         }
     }
 
+    function audioReset() {
+        audio.currentTime = 83.2;
+    }
+
+    select.addEventListener('click', function() {
+        audioReset();
+        select.classList.add('depress');
+        audioPlay();
+    }, false);
+
     logo.addEventListener('click', function() {
+        audioReset();
         logo.classList.add('pulsate');
-        play();
+        audioPlay();
+    }, false);
+
+    mute.addEventListener('click', function() {
+        audio.pause();
+        logo.classList.remove('pulsate');
+        select.classList.remove('depress');
+    }, false);
+
+    audio.addEventListener('canplay', function() {
+        audioReset();
     }, false);
 
     audio.addEventListener('ended', function() {
