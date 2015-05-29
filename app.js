71cb133a9faf14314d66107004bf1661e9dc85ae
Joe Violago <joe@violago.net>
2015-05-28 20:30:04 -0400

    whitespace fixes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 (function() {
 
-	var logo = document.getElementById('logo'),
-		audio = document.getElementById('audio'),
-		body = document.body;
+    var logo = document.getElementById('logo'),
+        audio = document.getElementById('audio'),
+        body = document.body;
 
-	function play() {
-		if (audio.paused) {
-			audio.play();
-		} else {
-			audio.currentTime = 0;
-		}
-	}
+    function play() {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.currentTime = 0;
+        }
+    }
 
-	logo.addEventListener('click', function() {
-		logo.classList.add('pulsate');
-		play();
-	}, false);
+    logo.addEventListener('click', function() {
+        logo.classList.add('pulsate');
+        play();
+    }, false);
 
-	audio.addEventListener('ended', function() {
-		logo.classList.remove('pulsate');
-	}, false);
+    audio.addEventListener('ended', function() {
+        logo.classList.remove('pulsate');
+    }, false);
 
 }());
