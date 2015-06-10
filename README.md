aedb909a372ab6a7055c2a953fcbad3aef70aa5d
Joe Violago <joev@Joes-MacBook-Pro.local>
2015-06-10 19:36:26 -0400

    MVP release: messy dump commit

--- README.md
+++ README.md
@@ -3,22 +3,56 @@ josephviolago.github.io
 
 Featurama standup chant
 
+## Inspiration
+https://www.youtube.com/watch?v=xA_goQ_qEMA
+
 ## Resources
 
 ### JS
 http://code.jquery.com/jquery-2.1.4.min.js
 
-### Images
+### CSS
+http://getbootstrap.com/css/
+http://necolas.github.io/normalize.css/
+
+### Assets
 http://upload.wikimedia.org/wikipedia/commons/8/84/Futurama_1999_logo.svg
 http://spot.pcc.edu/~gkerr/paststudents/matt_shettles/mysite/images/ship-large.png
 http://theinfosphere.org/images/f/fc/Planet_Express_Logo.svg
+
+### Background
+
+#### Resources
 http://datasplasher.stvisuals.com/media-hostpage/FUTURAMA-BACKGROUND-small.jpg
-http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg
+
+#### JS
+http://davidwalsh.name/javascript-spin
 
 ### Audio
-https://files.slack.com/files-pri/T02T0838D-F04SHATNS/download/futuramathemesong.mp3
 
-### Font
+#### Assets
+https://files.slack.com/files-pri/T02T0838D-F04SHATNS/download/futuramathemesong.mp3
+http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg
+
+#### JS
+http://www.w3schools.com/tags/ref_av_dom.asp
+http://stackoverflow.com/questions/7942452/javascript-slowly-decrease-volume-of-audio-element
+
+### Text
+
+#### Title
+
+##### Resources
 http://csswarp.eleqtriq.com/
 http://thefonty.com/font/futurama/download
 http://www.cufonfonts.com/en/font/4962/futurama
+
+##### JS
+http://tympanus.net/Development/Arctext/
+
+##### CSS
+http://jsfiddle.net/mbHB4/3/
+https://css-tricks.com/adding-stroke-to-web-text/
+
+#### Caption
+WIP
--- app.js
+++ app.js
@@ -3,44 +3,119 @@
     var logo = document.getElementById('logo'),
         audio = document.getElementById('audio')
         mute = document.getElementById('mute')
-        select = document.getElementById('nav');
+        select = document.querySelectorAll('#nav>li>button')
+        $checkpoint = '';
 
     function audioPlay() {
         if (audio.paused) {
+            audio.volume = 1;
             audio.play();
         } else {
             audioReset();
         }
     }
 
-    function audioReset() {
-        audio.currentTime = 83.2;
+    function audioReset(checkpointText) {
+        if (checkpointText == 'outro')  {
+            $checkpoint = 116.6;
+        } else {
+            $checkpoint = 83.2;
+        }
+
+        audio.currentTime = $checkpoint;
+        return $checkpoint;
     }
 
-    select.addEventListener('click', function() {
+    select[0].addEventListener('click', function() {
+        audio.pause();
         audioReset();
-        select.classList.add('depress');
-        audioPlay();
+        logo.classList.remove('pulsate');
+        select[0].classList.add('depress');
+        select[1].classList.remove('depress');
+    }, false);
+
+    select[1].addEventListener('click', function() {
+        audio.pause();
+        audioReset('outro');
+        logo.classList.remove('pulsate');
+        select[0].classList.remove('depress');
+        select[1].classList.add('depress');
     }, false);
 
     logo.addEventListener('click', function() {
-        audioReset();
         logo.classList.add('pulsate');
         audioPlay();
     }, false);
 
     mute.addEventListener('click', function() {
         audio.pause();
+        audioReset($checkpoint);
         logo.classList.remove('pulsate');
-        select.classList.remove('depress');
     }, false);
 
     audio.addEventListener('canplay', function() {
-        audioReset();
     }, false);
 
     audio.addEventListener('ended', function() {
         logo.classList.remove('pulsate');
     }, false);
 
+    select[0].click();
+
+    // every second your audio element is playing
+    $(audio).on('timeupdate', function() {
+        var vol = 1,
+        interval = 200; // 200ms interval
+
+        if (Math.floor(audio.currentTime) >= 117.8 ||
+            Math.floor(audio.currentTime) >= 85.3) {
+            if (audio.volume == 1) {
+                var intervalID = setInterval(function() {
+                    // Reduce volume by 0.05 as long as it is above 0
+                    // This works as long as you start with a multiple of 0.05!
+                    if (vol > 0) {
+                        vol -= 0.05;
+                        // limit to 2 decimal places
+                        // also converts to string, works ok
+                        audio.volume = vol.toFixed(2);
+                    } else {
+                        // Stop the setInterval when 0 is reached
+                        clearInterval(intervalID);
+                        mute.click();
+                    }
+                }, interval);
+            }
+        }
+    });
+
 }());
+
+google.load('webfont','1');
+
+google.setOnLoadCallback(function() {
+    WebFont.load({
+        google: {
+            families: ['Futurama Title Font']
+        },
+        fontactive : function(fontFamily, fontDescription) {
+            init();
+        },
+        fontinactive: function(fontFamily, fontDescription) {
+            init();
+        }
+    });
+});
+
+$('#warped').hide();
+var $titleMain = $('.title h1').hide();
+var $titleFill = $('.title p').hide();
+
+function init() {
+    // $title.show();
+    $titleMain.show().arctext({radius: 300});
+    $titleFill.show().arctext({radius: 300});
+}
+
+$(document).ready(function() {
+
+});
